aebb65b0648785aeeacba6cd37f94fc2625fdc15
Retky <jose.arredondo.a@outlook.com>
2022-02-04 10:34:57 -0600

    Create fucntion "playRps"
    function play a round and return win, lose or draw comparing player & 
    computer plays.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,42 @@
+// This will select the Computer Play.
 function computerPlay() {
-    let play = ['Rock', 'Paper', 'Scissors']
+    let play = ['rock', 'paper', 'scissors']
     return play[Math.floor(Math.random() * 3)]
 }
-console.log(computerPlay())
+function playRps(player) {
+    const playerChoose = player.toLowerCase();
+    let computerChoose = computerPlay();
+    console.log("player choose " + playerChoose)
+    console.log("computer choose " + computerChoose)
+    // rock > scissors | paper > rock | scissors > paper
+    switch (playerChoose) {
+        case "rock":
+            if (computerChoose == "scissors") {
+                return "You Win! " + playerChoose + " beats " + computerChoose
+            } else if (computerChoose == "paper") {
+                return "You Lose! " + computerChoose + " beats " + playerChoose
+            } else {
+                return "Draw"
+            }
+            break;
+        case "paper":
+            if (computerChoose == "rock") {
+                return "You Win! " + playerChoose + " beats " + computerChoose
+            } else if (computerChoose == "scissors") {
+                return "You Lose! " + computerChoose + " beats " + playerChoose
+            } else {
+                return "Draw"
+            }
+            break;
+        case "scissors":
+            if (computerChoose == "paper") {
+                return "You Win! " + playerChoose + " beats " + computerChoose
+            } else if (computerChoose == "rock") {
+                return "You Lose! " + computerChoose + " beats " + playerChoose
+            } else {
+                return "Draw"
+            }
+            break;
+    }
+}
+console.log(playRps("scissors"));
